2b292bf635404bc2183331dbbeaa209f29f8599f
TheRandom25 <edupurpose12@gmail.com>
2020-05-24 19:24:49 +0530

    6th commit

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,8 @@ import EditProfile from "./components/profile.forms/EditProfile";
 import AddExperience from "./components/profile.forms/AddExperience";
 import AddEducation from "./components/profile.forms/AddEducation";
 import PrivateRoute from "./components/routing/PrivateRoute";
-
+import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -47,6 +48,9 @@ const App = () => {
               //here goes all the routes that we're going to use in our front end
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
+
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               <PrivateRoute exact path="/edit-profile" component={EditProfile} />
